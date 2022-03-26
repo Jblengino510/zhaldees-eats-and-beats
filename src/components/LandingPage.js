6f8cae8c812abf8eb5210a2b7573bb6a41b5cc40
Joshua <j.blengino@yahoo.com>
2022-03-25 22:22:49 -0700

    more placeholder content added to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 
 function LandingPage() {
   return (
@@ -11,12 +15,71 @@ function LandingPage() {
           <Typography variant='h2' sx={{ml: '50px'}}>
             A safe & inclusive event space
           </Typography>
-          <Typography variant='h4' sx={{ml: '50px', mb: '50px', fontWeight: '12'}}>
+          <Typography variant='h4' sx={{ml: '50px', mb: '50px', fontWeight: '200'}}>
             serving the Alameda community.
           </Typography>
         </Box>
       </Box>
-      <Box sx={{height: '100vh', bgcolor: '#000000'}}>
+      <Box>
+        <Grid container direction='row' justifyContent='space-evenly' alignItems='center' textAlign='center' sx={{height: '100vh', bgcolor: '#000000', color: 'white', paddingTop: '300px'}}>
+          <Grid item xs={12} sx={{mb: '50px'}}>
+            <Typography variant='h2'>
+              A Blank Canvas
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sx={{mb: '50px'}}>
+            <Container>
+              <Typography variant='h5' sx={{fontWeight: '300'}}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: '350px' }}>
+              <CardMedia
+                component="img"
+                alt="image of DJ playing music"
+                height="350"
+                image="/djing.jpeg"
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: '350px' }}>
+                <CardMedia
+                  component="img"
+                  alt="image of 2 people settin up lights"
+                  height="350"
+                  image="/lighting.jpeg"
+                />
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: '350px' }}>
+                <CardMedia
+                  component="img"
+                  alt="image of a projector"
+                  height="350"
+                  image="/projector.jpeg"
+                />
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: '350px' }}>
+                <CardMedia
+                  component="img"
+                  alt="image of chairs"
+                  height="350"
+                  image="/chairs.jpeg"
+                />
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box>
+        <Grid container direction='row' justifyContent='center' alignItems='center' textAlign='center' sx={{height: '50vh', bgcolor:'#000000'}}>
+          <Grid item xs={12}></Grid>
+        </Grid>
       </Box>
     </>
   )
