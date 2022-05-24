0ac1bd776a041629991c5742a160177a68828653
Joshua <j.blengino@yahoo.com>
2022-05-24 09:21:04 -0700

    mobile burger menu added to navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,10 +7,32 @@ import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
 
 
 function NavBar() {
+    const [ value, setValue ] = useState(0)
+    const [ anchorEl, setAnchorEl ] = useState(null)
+    const open = Boolean(anchorEl);
     const navigate = useNavigate()
+
+    function handleClick(e){
+        setAnchorEl(e.currentTarget)
+    }
+
+
+    function handleClose(){
+       setAnchorEl(null)
+    }
+   
+
+    function handleChange(event, newValue) {
+        setValue(newValue)
+    }
+
 
 
   return (
@@ -18,9 +40,68 @@ function NavBar() {
         <AppBar position='sticky' elevation={0} sx={{background: '#000000', width: '100vw', padding: '20px'}}>
             <Toolbar>
                 <Box sx={{display: { sm: 'flex', md: 'none', lg: 'none'}}}>
-                    <IconButton color='secondary' size='large'>
+                    <IconButton color='secondary' size='large' aria-controls={open ? 'demo-positioned-menu' : undefined} aria-haspopup="true"  aria-expanded={open ? 'true' : undefined} onClick={handleClick}>
                         <MenuIcon sx={{ fontSize: 40 }}/>
                     </IconButton>
+                    <Menu anchorEl={anchorEl} open={open} onClose={handleClose} onClick={handleClose} transformOrigin={{ horizontal: 'right', vertical: 'top' }} anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                            },
+                            '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: '#222222',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                            },
+                            bgcolor: '#000000',
+                            padding: '5px',
+                            border: '2px solid #222222'
+                        },
+                        }}>
+                            <MenuItem sx={{bgcolor: '#000000',  paddingRight: '50px', '&:hover': {bgcolor: '#111111'}}}>
+                                <Button variant='text' color='secondary' size='large' onClick={() => navigate('/')} sx={{mr: '40px', borderRadius: '0px', '&:hover': {cursor: 'pointer', background: 'none', color: 'rgba(255,255,255,.4)'}}}>
+                                    <strong>Home</strong>
+                                </Button>
+                            </MenuItem>
+                            <Divider sx={{bgcolor: '#222222'}}/>
+                            <MenuItem sx={{bgcolor: '#000000',  paddingRight: '50px', '&:hover': {backgroundColor: '#111111'}}}>
+                                <Button variant='text' color='secondary' size='large' onClick={() => navigate('/about')} sx={{mr: '40px', borderRadius: '0px', '&:hover': {cursor: 'pointer', background: 'none', color: 'rgba(255,255,255,.4)'}}}>
+                                    <strong>About</strong>
+                                </Button>
+                            </MenuItem>
+                            <Divider sx={{bgcolor: '#222222'}}/>
+                            <MenuItem sx={{bgcolor: '#000000',  paddingRight: '50px', '&:hover': {backgroundColor: '#111111'}}}>
+                                <Button variant='text' color='secondary' size='large' onClick={() => navigate('/venue')} sx={{mr: '40px', borderRadius: '0px', '&:hover': {cursor: 'pointer', background: 'none', color: 'rgba(255,255,255,.4)'}}}>
+                                    <strong>Venue</strong>
+                                </Button>
+                            </MenuItem>
+                            <Divider sx={{bgcolor: '#222222'}}/>
+                            <MenuItem sx={{bgcolor: '#000000',  paddingRight: '50px', '&:hover': {backgroundColor: '#111111'}}}>
+                                <Button variant='text' color='secondary' size='large' href='https://www.zhaldeeseatsandbeats.com/' target='_blank' sx={{mr: '40px', borderRadius: '0px', '&:hover': {cursor: 'pointer', background: 'none', color: 'rgba(255,255,255,.4)'}}}>
+                                    <strong>Catering</strong>
+                                </Button>
+                            </MenuItem>
+                            <Divider sx={{bgcolor: '#222222'}}/>
+                            <MenuItem sx={{bgcolor: '#000000',  paddingRight: '50px', '&:hover': {backgroundColor: '#111111'}}}>
+                                <Button variant='text' color='secondary' size='large' onClick={() => navigate('/contact')} sx={{borderRadius: '0px', '&:hover': {cursor: 'pointer', background: 'none', color: 'rgba(255,255,255,.4)'}}}>
+                                    <strong>Contact</strong>
+                                </Button>
+                            </MenuItem>
+                        </Menu>
                 </Box>
                 <Grid container direction='column' justifyContent='center' alignItems='center'>
                     <Grid item xs={6} sm={6} md={12}>
